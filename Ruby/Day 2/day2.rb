87f9b685c57619600d4d526b321bb4c1b6ebbf29
Emma Lee-Choon <emma.lee.choon@johnlewis.co.uk>
2018-06-28 07:00:57 +0100

    Started tree exercise

--- Ruby/Day 2/day2.rb
+++ Ruby/Day 2/day2.rb
@@ -6,12 +6,28 @@
 # File.readlines("data.txt").each { |line| puts line}
 
 # A hash can be a two dimensional array: { 1 => 'one', 2 => 'two' } to [[1, 'one'], [2, 'two']]
-numbers = { 1 => 'one', 2 => 'two' }
+# numbers = { 1 => 'one', 2 => 'two' }
 
-def hash_to_array(hashToTranslate={})
-	a = []
-	hashToTranslate.keys.each { |key| a.push([key, hashToTranslate[key]]) }
-	puts a[0]
-end
+# def hash_to_array(hashToTranslate={})
+# 	a = []
+# 	hashToTranslate.each_pair { |key, value| a.push([key, value]) }
+# 	puts a.inspect
+# end
 
-hash_to_array(numbers)
+# hash_to_array(numbers)
+
+# An array can be mapped to a hash: [[1, 'one'], [2, 'two']] to { 1 => 'one', 2 => 'two' }
+# a = [[1, 'one'], [2, 'two']]
+
+# def array_to_hash(arrayToTranslate=[])
+# 	h = Hash[*arrayToTranslate.flatten(1)]
+# 	puts h.inspect
+# end
+
+# array_to_hash(a)
+
+# Print the contents of an array of 16 numbers, 4 at a time, using just each
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+# Print the contents of an array of 16 numbers, 4 at a time, using each_slice
+a.each_slice(4) { |i| p i }
